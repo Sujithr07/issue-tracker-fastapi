4d0ab13a0eb77e8ef5d5aae9327717f9f10cdf44
Sujith <sujithj7123@gmail.com>
2026-06-10 23:59:14 +0530

    basic item routing

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app= FastAPI()
 
+items= [
+    {"id":1, "name":"item one"}
+    {"id":2, "name":"item two"}
+    {"id":3, "name":"item three"}
+]
+
+
+@app.get ( "/items" )
+def get_items():
+    return items
+
+
+
 @app.get("/health")
 def health_check():
     return {"status":"ok"}
