25064e32ed61a98dbb3cd7a7fddd888334393eee
Sujith <sujithj7123@gmail.com>
2026-06-11 20:06:34 +0530

    issue get and post routes

--- app/routes/issues.py
+++ app/routes/issues.py
@@ -1,7 +1,47 @@
-from fastapi import APIRouter
+import uuid
+from fastapi import APIRouter, HTTPException, status
+from app.schemas import IssueCreate, IssueUpdate, IssueOut
+from app.storage import load_data, save_data
 
-router= APIRouter(prefix="/api/v1/issues",tags=["issues"])
+router = APIRouter(prefix="/api/v1/issues", tags=["Issues"])
 
-@router.get("/")
-async def get_issues():
-    return []
+
+@router.get("", response_model=list[IssueOut])
+def get_issues():
+    """Get all issues."""
+    issues = load_data()
+    return issues
+
+
+@router.get("/{issue_id}", response_model=IssueOut)
+def get_issue(issue_id: str):
+    """
+    Retrieve a specific issue by ID
+    """
+    issues = load_data()
+    for issue in issues:
+        if issue["id"] == issue_id:
+            return issue
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="Issue not found")
+
+@router.post("", response_model=IssueOut, status_code=status.HTTP_201_CREATED)
+def create_issue(payload: IssueCreate):
+    """
+    Create new issue
+    The issue is persisted to data/issues.json
+    """
+    issues = load_data()
+
+    issue = {
+        "id": str(uuid.uuid4()),
+        "title": payload.title,
+        "description": payload.description,
+        "priority": payload.priority.value,
+        "status": "open",
+    }
+
+    issues.append(issue)
+    save_data(issues)
+
+    return issue
